72c182310f933194c52596ec0dcb6e64a759317e
Furkan Demir <frndemir@gmail.com>
2022-04-02 17:39:42 +0300

    :recycle: refactoring...

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,3 +1,42 @@
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <div>dsadsa</div>;
+  const [loggedIn, setLoggedIn] = useState(null);
+
+  useEffect(() => {
+    const fetchProtected = async () => {
+      try {
+        await axios.get("/api/protected");
+        setLoggedIn(true);
+      } catch (error) {
+        setLoggedIn(false);
+      }
+    };
+
+    fetchProtected();
+  }, []);
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/auth/logout");
+      setLoggedIn(false);
+    } catch (error) {
+      alert("Something went wrong");
+    }
+  };
+
+  return loggedIn ? (
+    <div>
+      <h2>You're logged in!</h2>
+      <button onClick={logout}>Logout</button>
+    </div>
+  ) : (
+    <div>
+      <Link href="/login" passHref>
+        <button>Login</button>
+      </Link>
+    </div>
+  );
 }
--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -1,31 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    
+    event.preventDefault();
+
     const payload = {
       username,
-      password
-    }
+      password,
+    };
 
-    fetch('/api/login', {method: 'POST', body: payload})
-    .then((response) => response.json())
-    .then((response) => {
-      console.log(response)
-    }).catch((e) => alert(e))
-    
-  }
+    axios
+      .post("/api/auth/login", payload)
+      .then((response) => {
+        router.push("/");
+      })
+      .catch((error) => {
+        alert(error?.response?.data?.message || "Something went wrong");
+      });
+  };
 
   return (
     <div>
       <h1 style={{ marginBottom: 20 }}>Login</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" onChange={(event) => setUsername(event.target.value)} />
-        <input type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
+        <input
+          type="text"
+          placeholder="Username"
+          autoComplete="off"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          autoComplete="off"
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
